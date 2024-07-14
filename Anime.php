0626a40b59e398fc5dd3180e080639933ff9da01
Jodeel <joelkamau@stareheboyscentre.ac.ke>
2024-07-14 22:47:44 +0300

    commit1

---
+++ Anime.php
@@ -0,0 +1,92 @@
+<?php include "Includes\Header.php" ?>.
+<?php include "Includes\Navbar.php" ?>
+<div class="preview">
+<iframe class = "video" src="Resources\css\Pics\Best NEW Animation Movies Trailers_720p.mp4" frameborder="0.5"></iframe>
+<iframe class = "video" src="Resources\css\Pics\THE BEST NEW ANIMATION MOVIES 2024 Trailers_720p.mp4" frameborder="0.5"></iframe>
+  </div>
+<div class="boxes">
+    <div class="box1">
+<img src="Resources\css\Pics\Fr.jfif" alt=".">
+Frozen <br> <a href="Download.php"><svg style='font-size:50px;color:black' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+</svg></a>
+    </div>
+
+    <div class="box2">
+    <img src="Resources\css\Pics\Op.jfif" alt=".">
+Oppy<br> <a href="Download.php"><svg style='font-size:50px;color:black' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+</svg></a>
+    </div>
+
+    <div class="box3">
+    <img src="Resources\css\Pics\PoM.jfif" alt=".">
+Pengiuns of Madagascar <br> <a href="Download.php"><svg style='font-size:50px;color:black' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+</svg></a>        
+    </div>
+
+    <div class="box4">
+<img src="Resources\css\Pics\Rnm.jfif" alt=".">
+Rick and Morty <br> <a href="Download.php"><svg style='font-size:50px;color:black' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+</svg></a>
+    </div>
+
+    <div class="box5">
+<img src="Resources\css\Pics\Sb.jfif" alt=".">
+Spongebob Squarepants <br> <a href="Download.php"><svg style='font-size:50px;color:black' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+</svg></a>
+    </div>
+
+    <div class="box6">
+<img src="Resources\css\Pics\St.jfif" alt=".">
+Stitch <br> <a href="Download.php"><svg style='font-size:50px;color:black' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+</svg></a>
+    </div>
+
+    <div class="box7">
+<img src="Resources\css\Pics\Mb.jfif" alt=".">
+Meatballs <br> <a href="Download.php"><svg style='font-size:50px;color:black' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+</svg></a>
+    </div>
+
+    <div class="box8">
+<img src="Resources\css\Pics\Cr.jfif" alt=".">
+The Croods <br> <a href="Download.php"><svg style='font-size:50px;color:black' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+</svg></a>
+    </div>
+
+    <div class="box9">
+<img src="Resources\css\Pics\Up.jfif" alt=".">
+The Way UP <br> <a href="Download.php"><svg style='font-size:50px;color:black' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+</svg></a>
+    </div>
+
+    <div class="box10">
+<img src="Resources\css\Pics\Yt.jfif" alt=".">
+Yetty Rescue <br> <a href="Download.php"><svg style='font-size:50px;color:black' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z"/>
+</svg></a>
+    </div>
+</div>
+
+<script>
+</script>
+
+<?php include "Includes\Footer.php" ?>
---
+++ Download.php
@@ -0,0 +1,9 @@
+<?php include "Includes\Header.php" ?>.
+<?php include "Includes\Navbar.php" ?>
+<div class="Download">
+    <div class="trail">
+    <a href="#"style='color:white;font-size:40px'><i class="fas fa-video"style='font-size:40px;color:white'></i> Trailer</a><br>
+    <br><a href="#"style='color:white;font-size:40px'><i class="fas fa-download"style='font-size:40px;color:white'></i> Download</a>
+    </div>
+</div>
+<?php include "Includes\Footer.php" ?>
